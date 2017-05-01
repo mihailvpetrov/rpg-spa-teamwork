48e8edc8b7f7db93d34fa9d4ffdf5d042aadef02
Hristo Hristov <xpload@email.bg>
2017-05-01 18:56:27 +0300

    jQuery -> jquery

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var $ = require('jQuery');
+var $ = require('jquery');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
