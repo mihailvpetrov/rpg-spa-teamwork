0d6258e4b35f5aebdeee28ae00fdcb7d34479eb7
Hristo Hristov <xpload@email.bg>
2017-05-02 11:01:35 +0300

    added body-parser

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+// SETUP
+
 var express = require('express');
 var low = require('lowdb');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
+
+var app = express();
+var db = low('./database/database.json');
 
 var user = require('./routes/user');
 var character = require('./routes/character');
 var game = require('./routes/game');
 
-var app = express(),
-  db = low('./database/database.json');
+// CONFIGURATION
 
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use('/', user);
 app.use('/character', character);
 app.use('/game', game);
+
+// START
 
 app.listen(port, function () {
   console.log('Application is listening on port ' + port + "!");
